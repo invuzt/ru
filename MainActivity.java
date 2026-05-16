4383fc007261bd9c28897020915f78647b2057bd
invuzt <invuzt@gmail.com>
2026-05-16 08:45:35 +0000

    Add minimal Hello World APK with auto-sign

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.hello.world;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        TextView tv = new TextView(this);
+        tv.setText("Hello World");
+        setContentView(tv);
+    }
+}
